77db66df688b3757effa3d702f88d00b1bf8c839
Hank <yaohan97@foxmail.com>
2024-12-25 16:27:37 +0800

    fix: Update cursor rules and HTML structure; enhance Vite config and main styles
    - Modified cursor rules to clarify limitations on tab navigation and read-only cell behavior.
    - Cleaned up HTML structure for better readability.
    - Adjusted Vite configuration formatting for consistency.
    - Added custom styles to override SmartUI theme for improved UI appearance.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import App from './App.tsx'
 import './index.css'
 import 'smart-webcomponents-react/source/styles/smart.default.css';
 import './assets/css/reset.scss'
+// 覆盖smart-ui的样式
+import './styles/smart-ui-theme.scss'
+
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,21 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
     },
-  },
-  server: {
-    hmr: true,
-    // open: true,
-    cors: true,
-    port: 6868,
-    host: "0.0.0.0",
-    // https: {
-    //     cert: "./localhost.crt",
-    //     key: "./localhost.key"
-    // },
-},
+    server: {
+        hmr: true,
+        // open: true,
+        cors: true,
+        port: 6868,
+        host: "0.0.0.0",
+        // https: {
+        //     cert: "./localhost.crt",
+        //     key: "./localhost.key"
+        // },
+    }
 })
